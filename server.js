b8eb219655a3f04fb181d907560bdfff451bdd41
gordonmaloney <gordonmaloney@gmail.com>
2023-07-06 15:44:21 +0100

    cors

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    origin: "https://louderthanwords.netlify.app/",
+    origin: "https://louderthanwords.netlify.app",
   })
 );
 
